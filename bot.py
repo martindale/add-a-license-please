22be76abdd3b2df9996909cbb95f2fadb3e51580
Karan Goel <karan@goel.io>
2015-04-10 07:27:41 -0700

    add pymongo, add searching

--- bot.py
+++ bot.py
@@ -2,14 +2,27 @@
 
 
 # Third-party dependencies
+from pymongo import MongoClient
 import requests
+
 
 # Custom imports
 import config
 
 
-def make_request(endpoint, req_type='get', headers={}, body={}):
-    '''Make a HTTP request
+client = MongoClient('localhost', 23467)
+db = client.add_a_license_db
+
+
+def make_request(endpoint, req_type='get', headers={}, params={}, body={}):
+    '''Make a HTTP request and return JSON response
+
+    Arguments:
+        endpoint: Github endpoint to hit
+        req_type: HTTP request type
+        headers: Any custom headers that must be sent
+        params: 'get' query parameters
+        body: 'post' body content
     '''
 
     if not headers.has_key('Authorization'):
@@ -18,12 +31,26 @@ def make_request(endpoint, req_type='get', headers={}, body={}):
     url = config.github['base_url'] + endpoint
 
     if req_type is 'get':
-        r = requests.get(url, headers=headers)
+        r = requests.get(url, headers=headers, params=params)
     elif req_type is 'post':
         r = requests.post(url, headers=headers, payload=body)
 
-    return r
+    try:
+        return r.json()
+    except e:
+        # TODO log this
+        return None
 
 
-r = make_request('/rate_limit')
-print r.text
+def get_search_results():
+    params = {'q': 'stars:"<5 AND >1"', 'sort': 'updated'}
+    r = make_request('/search/repositories', params=params)
+    print r['items'][0]['full_name']
+
+
+def main():
+    get_search_results()
+
+
+if __name__ == '__main__':
+    main()
